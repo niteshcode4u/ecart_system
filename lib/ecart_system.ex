a8ae7b54a1b6050a3f8110f54a50cba185291afb
virusnitesh <nitesh.ranjan.gaya@gmail.com>
2022-10-16 01:33:27 +0530

    Add Ecart Supervisor & Manager for scanning products and price calculation

--- lib/ecart_system.ex
+++ lib/ecart_system.ex
@@ -3,16 +3,67 @@ defmodule EcartSystem do
   Documentation for `EcartSystem`.
   """
 
+  alias EcartSystem.EcartSupervisor
+  alias EcartSystem.Managers.EcartManager
+
+  @spec start_transaction :: {:error, String.t()} | {:ok, String.t()}
   @doc """
-  Hello world.
+    Starts the transaction to add product into the cart or throw error if already exist
 
-  ## Examples
+    ## Examples
 
-      iex> EcartSystem.hello()
-      :world
+      iex> EcartSupervisor.start_transaction()
+      {:ok, "Trasaction started"}
+
+      iex> EcartSupervisor.start_transaction()
+      {:error, "Already exists. Please complete the transaction first."}
 
   """
-  def hello do
-    :world
-  end
+  defdelegate start_transaction, to: EcartSupervisor
+
+  @spec complete_transaction :: {:ok, String.t()} | {:error, String.t()}
+  @doc """
+    Completed the transaction for the available items into cart
+
+    ## Examples
+
+      iex> EcartSupervisor.complete_transaction()
+      {:ok, "Transaction completed"}
+
+      iex> EcartSupervisor.complete_transaction()
+      {:error, "No child found"}
+
+  """
+  defdelegate complete_transaction, to: EcartSupervisor
+
+  @spec scan_product(String.t()) :: :ok
+  @doc """
+    Scan product/ item one by one and return :ok in response
+
+    ## Examples
+
+      iex> EcartManager.scan_product("VOUCHER")
+      :ok
+
+      iex> EcartManager.scan_product("TSHIRT")
+      :ok
+
+      iex> EcartManager.scan_product("MUG")
+      :ok
+
+  """
+  defdelegate scan_product(product_code), to: EcartManager
+
+  @spec calculate_total :: String.t()
+  @doc """
+    Calculate total price by adding offers and return total amount in EUR
+
+    Please note we are considering all amounts in currency EUR
+    ## Examples
+
+      iex> EcartManager.calculate_total()
+      "0€"
+
+  """
+  defdelegate calculate_total, to: EcartManager
 end
---
+++ lib/managers/ecart_manager.ex
@@ -0,0 +1,87 @@
+defmodule EcartSystem.Managers.EcartManager do
+  @moduledoc """
+  To handle the state of shopping cart
+  """
+
+  # Structure for state is %{voucher: 0, mug: 0, tshirt: 0}
+
+  use GenServer, restart: :transient
+
+  import EcartSystem.Product, only: [fetch_prices: 0]
+
+  # Client APIs #############################################################
+
+  @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid}
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  @spec get_ecart_session :: {:ok, map()}
+  def get_ecart_session do
+    GenServer.call(__MODULE__, :get_ecart_session)
+  end
+
+  @spec scan_product(String.t()) :: :ok
+  def scan_product(product_code) do
+    GenServer.cast(__MODULE__, {:scan_product, product_code})
+  end
+
+  @spec calculate_total :: String.t()
+  def calculate_total do
+    GenServer.call(__MODULE__, :calculate_total)
+  end
+
+  # Call Handlers #############################################################
+
+  @impl GenServer
+  @spec init(any) :: {:ok, %{}}
+  def init(_opts) do
+    {:ok, %{}}
+  end
+
+  @impl GenServer
+  def handle_call(:get_ecart_session, _from, state) do
+    products = Map.keys(fetch_prices())
+
+    state =
+      state
+      |> Enum.filter(fn {product, _quantity} ->
+        product in products
+      end)
+      |> Map.new()
+
+    {:reply, {:ok, state}, state}
+  end
+
+  @impl GenServer
+  def handle_call(:calculate_total, _from, state) do
+    total_amount = calculate_total_amount(state)
+    {:reply, "#{total_amount}€", state}
+  end
+
+  @impl GenServer
+  def handle_cast({:scan_product, product_code}, state) do
+    product_code = product_code |> String.downcase() |> String.to_atom()
+
+    if product_code in Map.keys(fetch_prices()) do
+      {:noreply, Map.put(state, product_code, (state[product_code] || 0) + 1)}
+    else
+      {:noreply, state}
+    end
+  end
+
+  # Private functions #############################################################
+
+  defp calculate_total_amount(products) do
+    # price = EcartSystem.Product.fetch_prices()
+
+    Enum.reduce(products, 0, fn {product, quantity}, total ->
+      product_cost = pricing_rule(product, quantity, fetch_prices())
+      product_cost + total
+    end)
+  end
+
+  defp pricing_rule(:voucher, quantity, price), do: round(quantity / 2) * price.voucher
+  defp pricing_rule(:tshirt, quantity, price) when quantity > 2, do: quantity * (price.tshirt - 1)
+  defp pricing_rule(product, quantity, price), do: quantity * price[product]
+end
